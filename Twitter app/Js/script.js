e01485a711eef3ac11a3d10fdc5fd0efbdcbaf55
azizulbappy0096 <azizulbappy6@gmail.com>
2020-09-22 21:49:23 +0600

    tweet container done

--- Twitter app/Js/script.js
+++ Twitter app/Js/script.js
@@ -29,35 +29,89 @@ document.getElementById("user-input").addEventListener("keyup", eventHandler);
 displayTwitterData = (response) => {
     let text = '';
     response["statuses"].map(data => {
+        let date = new Date(data.created_at).toDateString()
+        const createdDate = moment(date).fromNow();
         text += `
             <div class="tweets-container">
                     <div class="tweets-user-info">
-                        <div class="tweet-user-profile-image">
+                        <div class="tweet-user-profile-image" style="background-image: url(${data.user.profile_image_url_https})">
 
                         </div>
                         <div class="tweet-user-name-container">
                             <p class="tweet-user-fullName">
-                                Azizul Bappy
+                                ${data.user.name}
                             </p>
                             <p class="tweet-user-userName">
-                                @bappy_azizul
+                                @${data.user.screen_name}
                             </p>
                         </div>
-                    </div>
-                    <div class="tweets-contents">
+                    </div>`;
 
-                    </div>
-                    <div class="tweets-text">
+                    if(data["extended_entities"]) {
+                        text += `${buildImages(data["extended_entities"]["media"])}`;
+                        text += `${buildVideo(data["extended_entities"]["media"])}`
+                    }
+
+                    text +=
+                    `<div class="tweets-text">
                         <p>
                             ${data["full_text"]}
                         </p>
                     </div>
                     <div class="tweet-date">
-                        <p>20 Jan,2020</p>
+                        <p> ${createdDate}</p>
                     </div>
             </div>
         `
     });
 
     document.querySelector(".tweets-list").innerHTML = text;
+}
+
+buildImages = (metaData) => {
+    const end = (metaData.length < 4) ? metaData.length:4;
+    let img = '<div class="tweets-contents">';
+    let imageExists = false;
+    for(let i = 0; i < end; i++) {
+        if(metaData[i]["type"] === "photo") {
+            imageExists = true;
+            img += `
+            <div class="tweet-image" style="background-image: url(${metaData[i]["media_url_https"]})"></div>
+            `
+        }
+    }
+    img += "</div>";
+    return imageExists ? img : "";
+}
+
+buildVideo = (metaData) => {
+    let video = '<div class="tweets-contents">';
+    let videoExists = false;
+    for(let i = 0; i < 1; i++) {
+        if(metaData[i]["type"] === "video") {
+            videoExists = true;
+            const videoUrl = metaData[i]["video_info"]["variants"].map(video => {
+                return (video["content_type"] === "video/mp4") ? video["url"] : "";
+            });
+            videoUrl.filter(getUrl => getUrl !== "")
+                video += `
+                <video controls class="tweet-video">
+                    <source src=${videoUrl[0]} type="video/mp4">
+                </video>
+                `;
+        }else if(metaData[i].type === "animated_gif") {
+            videoExists = true;
+            const videoUrl = metaData[i]["video_info"]["variants"].map(video => {
+                return (video["content_type"] === "video/mp4") ? video["url"] : "";
+            });
+            videoUrl.filter(getUrl => getUrl !== "")
+                video += `
+                <video class="tweet-video" autoplay loop>
+                    <source src=${videoUrl[0]} type="video/mp4">
+                </video>
+                `;
+        }
+    }
+    video += "</div>";
+    return videoExists ? video : "";
 }
